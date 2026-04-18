78504c7cac3dd47352971ee42ecc4708e560de36
emmanuel <emmanuel.devpro@gmail.com>
2026-04-18 22:44:09 +0100

    added profile image, installed framer motion, added paddings to the body in the global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <main className="border-2 border-blue-500 flex flex-col flex-1 bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
         <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Welcome to Emmanuel I. Chukwu Portfolio! 👋
+          Hi, I&apos;m Emmanuel Chukwu.
         </h1>
       </div>
-    </div>
+    </main>
   );
 }
